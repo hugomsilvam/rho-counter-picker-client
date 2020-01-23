8c422e727bf22070f681281b8e63fef856514fd8
Hugo Silva <hugomsilva@ua.pt>
2020-01-23 11:05:16 +0000

    start implementing websocket client connection

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-export const backendDomain = "https://intense-basin-14122.herokuapp.com";
-//export const backendDomain = "http://localhost:8080"
+//export const backendDomain = "https://intense-basin-14122.herokuapp.com";
+export const backendDomain = "http://localhost:8080"
 
-export default backendDomain;
+//export const websocketDomain = "ws://intense-basin-14122.herokuapp.com:3000";
+export const websocketDomain = "ws://localhost:8080/ws";
+export default { backendDomain, websocketDomain };
